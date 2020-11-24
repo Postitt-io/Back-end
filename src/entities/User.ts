5cab8d221205ef320936b90f99a0a3a272524169
Nathan Mayall <nathanmayall@icloud.com>
2020-11-24 23:02:03 +0000

    add create user & validation

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,18 +1,52 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { IsEmail, Length } from "class-validator";
+import { Index } from "typeorm";
+import { BeforeInsert } from "typeorm";
+import { UpdateDateColumn } from "typeorm";
+import { CreateDateColumn } from "typeorm";
+import { BaseEntity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-@Entity()
-export class User {
+import bcrypt from "bcrypt";
+import { classToPlain, Exclude } from "class-transformer";
 
-    @PrimaryGeneratedColumn()
-    id: number;
+@Entity("users")
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
 
-    @Column()
-    firstName: string;
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    lastName: string;
+  @Index()
+  @Length(3, 255, { message: "Username must be at least 3 characters long" })
+  @Column({ unique: true })
+  username: string;
 
-    @Column()
-    age: number;
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
 
+  @Exclude()
+  @Column()
+  @Length(6, 255, { message: "Password must be at least 6 characters long" })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,27 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entities/User";
+import { createConnection } from "typeorm";
+import express from "express";
+import morgan from "morgan";
 
-createConnection().then(async connection => {
+import authRoutes from "./routes/auth";
+import trim from "./middleware/trim";
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const app = express();
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(trim);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+app.get("/", (_, res) => res.send("Hello World"));
+app.use("/api/auth", authRoutes);
 
-}).catch(error => console.log(error));
+app.listen(5000, async () => {
+  console.log("Server running at http://localhost:5000");
+
+  try {
+    await createConnection();
+    console.log("Database Connected");
+  } catch (error) {
+    console.log(error);
+  }
+});
