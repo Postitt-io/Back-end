c9c478a7cd89617e13de7cba0cbb64ea1765cae5
Nathan M <nathanmayall@icloud.com>
2021-01-09 23:03:58 +0000

    add route and handler to search for sub

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -136,10 +136,32 @@ const uploadSubImage = async (req: Request, res: Response) => {
   }
 };
 
+const searchForSub = async (req: Request, res: Response) => {
+  try {
+    const name = req.params.name;
+    if (isEmpty(name)) {
+      return res.status(400).json({ error: "Name must not be empty" });
+    }
+
+    const subs = await getRepository(Sub)
+      .createQueryBuilder()
+      // if you want to seatch for react sub, search rea and it will find it as it's part of the string
+      .where("LOWER(name) LIKE :name", {
+        name: `%${name.toLowerCase().trim()}%`,
+      })
+      .getMany();
+    return res.json(subs);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
 const router = Router();
 
 router.post("/", user, auth, createSub);
-router.get("/:name/", user, getSub);
+router.get("/:name", user, getSub);
+router.get("/search/:name", searchForSub);
 router.post(
   "/:name/image",
   user,
